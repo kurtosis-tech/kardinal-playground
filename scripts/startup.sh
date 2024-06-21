ec523f53e776e6427364e763e4e1c5b8b3aa8181
Gyanendra Mishra <anomaly.the@gmail.com>
2024-06-21 14:00:57 +0100

    eject early eject often

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -euo pipefail
 
 # Get the total memory in bytes
 total_memory=$(awk '/MemTotal/ {print $2}' /proc/meminfo)
@@ -29,12 +30,6 @@ kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.22/samp
 kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.22/samples/addons/jaeger.yaml
 kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.22/samples/addons/kiali.yaml
 kubectl rollout status deployment/kiali -n istio-system
-
-# Start Minikube dashboard
-minikube dashboard &
-
-# Start Kiali dashboard
-istioctl dashboard kiali &
 
 # Add hostnames to /etc/hosts
 echo "127.0.0.1 voting-app.localhost" | sudo tee -a /etc/hosts
